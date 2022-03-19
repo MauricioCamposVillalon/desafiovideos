81d274092e1cd67316f553d2c8cb7d6829acf581
Mauricio Campos <thoyferr@gmail.com>
2022-03-19 03:12:11 -0300

    finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,29 @@
+document.getElementById("framevideo").style.display = "none";
+
+
+
+const btnMusica = document.querySelector('#headingOne');
+const btnPelicula = document.querySelector('#headingTwo');
+const btnSerie = document.querySelector('#headingThree');
+let estado = '';
+
+btnMusica.addEventListener('click', () => {
+    estado = '0';
+    playMultimedia();
+});
+
+btnPelicula.addEventListener('click', () => {
+    estado = '1';
+    playMultimedia();
+});
+
+btnSerie.addEventListener('click', () => {
+    estado = '2';
+    playMultimedia();
+});
+
+
+
 class Multimedia {
     constructor(url) {
         this._url = url;
@@ -26,33 +52,42 @@ class Reproductor extends Multimedia {
 const arregloBuscar = [];
 const mus = new Reproductor('https://www.youtube.com/embed/naLB5aQmWco', 'musica');
 arregloBuscar.push(mus);
-const pel = new Reproductor('https://www.youtube.com/watch?v=pIhlDew9qbs', 'pelicula');
+const pel = new Reproductor('https://www.youtube.com/embed/Z6LXG2TJPKg', 'pelicula');
 arregloBuscar.push(pel);
 const ser = new Reproductor('https://www.youtube.com/embed/aGy9x29vzxo', 'serie');
 arregloBuscar.push(ser);
 
+
 const funcionPublica = () => {
     const funcionPrivada = () => {
-
-        let tim = arregloBuscar[0]._url;
+        let tim = arregloBuscar[estado]._url;
         tim += '?start=';
-        tim += arregloBuscar[0].setInicio();
-        //console.log(arregloBuscar[0]._url,arregloBuscar[0]._id,tim);
-        const musica = document.querySelector('#framevideo');
-        console.log(tim)
-        musica.setAttribute('src', `${tim}`);
-        console.log(hola);
+        tim += arregloBuscar[estado].setInicio();
+        if (estado === '0') {
+            document.getElementById("framevideo").style.display = "block";
+            let opc = document.querySelector('#framevideo');
+            opc.setAttribute('src', `${tim}`);
+        } else if (estado === '1') {
+            let opc2 = document.querySelector('#framepeli');
+            opc2.setAttribute('src', `${tim}`);
+            console.log(tim);
+
+        } else if (estado === '2') {
+            let opc3 = document.querySelector('#frameserie');
+            opc3.setAttribute('src', `${tim}`);
+            console.log(tim);
+        }
     }
     return funcionPrivada;
 }
-
 
 
 const playMultimedia = funcionPublica();
 
 
 
-playMultimedia();
+
+
 
 
 
